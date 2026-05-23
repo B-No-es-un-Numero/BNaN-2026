b1597af4a1b4e2194c566912073fb5f63da2b710
Guille\Guillermo <guillermodivan@hotmail.com>
2026-05-23 08:45:19 -0300

    feat(front):131-filter-clients-added

--- frontend/bnan-project/src/app/pages/user-dashboard/client-list-view/client-list-view.ts
+++ frontend/bnan-project/src/app/pages/user-dashboard/client-list-view/client-list-view.ts
@@ -1,25 +1,62 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { ClientService } from '../../../services/client-service';
 
 @Component({
   selector: 'app-client-list-view',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './client-list-view.html',
   styleUrl: './client-list-view.css',
 })
 export class ClientListView implements OnInit {
-   clientList= signal<any>({} as any);
-   private clientService= inject(ClientService);
+
+  private clientService = inject(ClientService);
+
+  clientList = signal<any[]>([]);
+  searchTerm = signal<string>('');
+  filteredClientList = computed(() => {
+    const term = this.searchTerm().toLowerCase().trim();
+
+    if (!term) {
+      return this.clientList();
+    }
+
+    return this.clientList().filter((client) => {
+
+      const name = client.name?.toLowerCase() || '';
+      const email = client.email?.toLowerCase() || '';
+      const company = client.company_name?.toLowerCase() || '';
+      const responsible = client.responsible_name?.toLowerCase() || '';
+      const statusMap: Record<string, string> = {
+        active: 'activo',
+        lead: 'lead',
+        closed: 'cerrado'
+      };
+      const status = statusMap[client.status?.toLowerCase()] || '';
+      
+      return (
+        name.includes(term) ||
+        email.includes(term) ||
+        company.includes(term) ||
+        status.includes(term) ||
+        responsible.includes(term)
+      );
+    });
+  });
 
   ngOnInit(): void {
     this.clientService.getClientList().subscribe({
-      next: (data) => {
+      next: (data: any) => {
         this.clientList.set(data);
       },
       error: (error) => console.error(error),
       complete: () => console.info('complete')
-    }) 
+    });
+  }
 
-}
+  onSearch(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.searchTerm.set(input.value);
+  }
 }
