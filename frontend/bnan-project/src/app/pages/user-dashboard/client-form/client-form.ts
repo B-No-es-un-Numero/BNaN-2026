4fff8d6c63ae24fe3c3fa49a3952dc5815a8dfc3
Bruvm <melibruvera@hotmail.com>
2026-05-25 20:04:30 -0300

    alert was changed to toast

--- frontend/bnan-project/src/app/pages/user-dashboard/client-form/client-form.ts
+++ frontend/bnan-project/src/app/pages/user-dashboard/client-form/client-form.ts
@@ -1,18 +1,30 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { ClientService } from '../../../services/client/client-service';
 import { CreateClientRequest } from '../../../model/client.model';
+import { Toast } from '../../../shared/toast/toast/toast';
 
 @Component({
   selector: 'app-client-form',
-  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, Toast],
   templateUrl: './client-form.html',
 })
 export class ClientForm {
   private fb = inject(FormBuilder);
   private clientService = inject(ClientService);
+
+  toastOpen = signal(false);
+  toastMessage = signal('');
+  toastType = signal<'success' | 'error' | 'info'>('success');
+
+  showToast(message: string, type: 'success' | 'error' | 'info') {
+    this.toastMessage.set(message);
+    this.toastType.set(type);
+    this.toastOpen.set(true);
+    setTimeout(() => this.toastOpen.set(false), 4000);
+  }
 
   form = this.fb.nonNullable.group({
     name: [
@@ -37,7 +49,7 @@ export class ClientForm {
   }
   this.clientService.createClient(this.form.getRawValue() as CreateClientRequest)
     .subscribe({ next: () => {
-        alert('El cliente se registró exitosamente');
+        this.showToast('El cliente se registró exitosamente', 'success');
         this.form.reset();
       },
       error: (error) => {console.error(error); }
