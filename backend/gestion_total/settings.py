38409fd473cb412b15824ce88270a9becad73ef7
Guille\Guillermo <guillermodivan@hotmail.com>
2026-06-01 13:42:19 -0300

    fix/backend(nnn): suggested changes, enums and names-db, cuit

--- backend/gestion_total/settings.py
+++ backend/gestion_total/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True 
+CORS_ORIGIN_ALLOW_ALL = 'True '
 
 ROOT_URLCONF = 'gestion_total.urls'
 
